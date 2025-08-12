65f78fe31c205e8357930d5e59f61937b242ff85
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-12 10:22:52 +0530

    Remove commented-out console.log statements and clean up object examples in hello.js

--- day7/hello.js
+++ day7/hello.js
@@ -13,3 +13,23 @@
 
 //  console.log(d)
 //  console.log(f)
+
+
+// const obj ={"student":"RBMI",
+//         "marks":{
+//             "maths":45,
+//             "React":50
+//         },
+//         "location":"up"
+//}
+// console.log(obj.student)
+// console.log(obj.marks)
+
+// const{marks:{maths,React}}=obj
+// console.log(obj);
+// console.log(student);
+// console.log(maths);
+// console.log(React);
+
+
+
