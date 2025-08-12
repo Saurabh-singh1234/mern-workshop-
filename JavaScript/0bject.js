74a956a6e91bd744e44bb771f02af7460afebca5
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-12 09:32:16 +0530

    Fix script source path in indexclass.html; update 0bject.js comments for clarity

--- JavaScript/0bject.js
+++ JavaScript/0bject.js
@@ -4,6 +4,8 @@
 //     }
 // };
 
+// const { useCallback } = require("react");
+
 // const karanArjun ={
 //     salary :234 ,
 //     calTax() {
@@ -16,3 +18,121 @@
 //           class
 
 
+// class car {
+//     start(){
+//         console.log("start");
+//     }
+//      stop(){
+//         console.log("stop");
+//     }
+// }
+
+// let fortuner = new car();
+
+// fortuner.start();
+// fortuner.stop();
+
+
+// let DATA ="secret inforation"
+// class User {
+//     constructor (name ,email){
+//         this.name = name;
+//         this.email = email;
+
+//     } 
+
+//     viewData(){
+//         console.log("data =" ,DATA);
+//     }
+// }
+
+// class Admin extends User{
+//     constructor (name ,email){
+//         super(name , email);
+//     }
+//     editData(){
+//         DATA ="some new data";
+//     }
+// }
+
+// let student1 = new User("saurabh", "saurabhsingh10761@gmail.com");
+// let student2 = new User("rohit", "rohitsingh10761@gmail.com");
+
+// let Admin1 = new Admin("nitin" ,"admin@gamial.com");
+
+
+
+// let a=1;
+// let b=2;
+
+// console.log("a+b=" ,a+b);
+// console.log("a+b=" ,a+b);
+// console.log("a+b=" ,a+b);
+// try{
+//     console.log("a+b=" ,a+c);
+// }catch(err)
+// {
+//     console.log(err);
+// }
+// console.log("a+b=" ,a+b);
+// console.log("a+b=" ,a+b);
+// console.log("a+b=" ,a+b);
+
+
+                //Asynchronous programing=>-----------------------
+// =>______________________________________________-----------------
+
+    // function hello(){
+    //     console.log("hello");
+    // }
+
+    // setTimeout(hello ,4000);       // 2m => 2000ms   time
+
+    // console.log("one");
+
+    // setTimeout(() => {
+    //     console.log("hello");
+        
+    // }, 4000);
+        
+    //  console.log("two");
+    //   console.log("three");
+
+
+                //Callback
+//-----------------------------------------------
+//----------------------------------------------- 
+
+// function sum (a,b){
+//     console.log(a+b);
+// }
+
+// function calculator(a ,b ,sumCallback){
+//         sumCallback(a ,b);
+// }
+
+// calculator(1 ,2 ,sum);
+
+
+
+
+//=============--------------------
+
+// function getData(dataId ,getNextData){
+//     setTimeout(() => {
+//         console.log("data" ,dataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+        
+//     },4000);
+// }
+
+            // callback hell-----------
+            //----------------------
+            
+// getData(1,()=>{
+//     getData(2,()=>{
+//         getData(3);
+//     });
+// });
