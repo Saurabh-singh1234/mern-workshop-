1202934c2db52b8be7009141d712852451e4e59e
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-12 10:04:32 +0530

    Add initial content to hello.js for destructuring examples

---
+++ day7/hello.js
@@ -0,0 +1,15 @@
+ // destructuring
+
+//  const arr ={"one":"hello" ,"two":"my" ,"three":"bye"}
+ // const one =arr[0]
+
+//  const {one ,two ,three }=arr
+//  console.log(one)
+//  console.log(two)
+//  console.log(three)
+
+//  const hello =[1,2,5]
+//  const [d,f] =hello
+
+//  console.log(d)
+//  console.log(f)
