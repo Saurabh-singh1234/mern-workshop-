5d3f0cab898346099cfe3d0f404ce4fdb4997f2c
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-12 20:10:05 +0530

    Add fetch functionality and define getFacts function in api.js

---
+++ JavaScript/API/api.js
@@ -0,0 +1,7 @@
+const URL ="https://www.ibm.com/downloads/documents/us-en";
+let promise = fetch(URL);
+console.log(promise);
+
+const getFacts = asyns () => {
+         console.log("getting data...");
+}
