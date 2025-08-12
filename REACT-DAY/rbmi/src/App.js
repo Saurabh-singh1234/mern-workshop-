b2dfc958d6c7ab8316d265f9982e9ff8f1d71fcb
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-12 19:44:10 +0530

    Refactor App.js to remove unused imports and simplify the component structure

--- REACT-DAY/rbmi/src/App.js
+++ REACT-DAY/rbmi/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+  // const hi="hello"
+  const hi=true
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {hi?<h1>my name saurabh</h1>:<h6>hello</h6>}
+
     </div>
+
   );
 }
-
-export default App;
+export default App
