48edca9e879abc3f29d7850f8a0ced3658cb1cbe
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-12 20:59:33 +0530

    Fix typo in getFacts function and enhance API interaction; add button and paragraph elements in index.html

--- JavaScript/API/api.js
+++ JavaScript/API/api.js
@@ -1,7 +1,17 @@
 const URL ="https://www.ibm.com/downloads/documents/us-en";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
 let promise = fetch(URL);
 console.log(promise);
 
-const getFacts = asyns () => {
+const getFacts = async () => {
          console.log("getting data...");
-}
+          let response = await fetch(URL);
+          console.log(response);  // JSON fomat
+          let data  = await response.json();
+          console.log(data[0].text);
+          factPara.innerText = data[2].text
+}; 
+
+
+btn.addEventListener("click",getFacts);
