0b0ba8f1795be0462e6823a338bf44042ec578ff
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-12 09:32:01 +0530

    Refactor employee and karanArjun object declarations in 0bject.js; add indexclass.html for script inclusion

--- JavaScript/0bject.js
+++ JavaScript/0bject.js
@@ -1,9 +1,18 @@
-const employee ={
-    calTax(){
-        console.log("tax rate is 10%");
-    }
-};
+// const employee ={
+//     calTax(){
+//         console.log("tax rate is 10%");
+//     }
+// };
 
-const karanArjun ={
-    
-}
+// const karanArjun ={
+//     salary :234 ,
+//     calTax() {
+//         console.log("tax rate is 20%");
+//     }
+// };
+// karanArjun._proto_ = employee;
+
+
+//           class
+
+
