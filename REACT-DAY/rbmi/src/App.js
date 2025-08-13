440d4122042ccb32ea385980632ca0ba1cb027b9
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-13 12:26:42 +0530

    Refactor App component structure and CSS class for layout consistency

--- REACT-DAY/rbmi/src/App.js
+++ REACT-DAY/rbmi/src/App.js
@@ -7,7 +7,12 @@ function App() {
   
     
   return (
-        
+        <div class="my">
+          <form>
+            <button>SEARCH</button>
+            <button>CONTECT US</button>
+          </form>
+      </div>        
   
   );
 }
