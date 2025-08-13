77c7ac4e5447a26f0b45826329089654a7018882
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-13 14:23:10 +0530

    Refactor App component layout and remove unused CSS styles

--- REACT-DAY/rbmi/src/App.js
+++ REACT-DAY/rbmi/src/App.js
@@ -1,16 +1,21 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
  
 
 function App() {
   
-  
+  const li =["apple","mango","banna"]
+  const [data,setData]=useState("logged in")
     
   return (
         <div class="my">
           <form>
-            <button>SEARCH</button>
-            <button>CONTECT US</button>
+           <ul>
+            {li.map(element=>(
+                <li>{element}</li>
+            ))}
+           </ul>
           </form>
       </div>        
   
