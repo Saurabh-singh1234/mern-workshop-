84a9b1682860514083f1091684419f594f687821
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-13 12:01:31 +0530

    Implement currency dropdown population and flag update functionality

--- JavaScript/CURREN-CONVERTER/calculator.js
+++ JavaScript/CURREN-CONVERTER/calculator.js
@@ -2,6 +2,42 @@ const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/
 
 const  dropdowns = document.querySelectorAll(".dropdown select");
 
-let (code in countryList) {
-    console.log(code);
+// for (code in countryList) {
+//     console.log(code ,countryList[code]);
+// }
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        if(select.name==="form" && currCode=== "USD"){
+            newOption.selected= "selected";
+        }else if(select.name==="to" && currCode=== "INR"){
+            newOption.selected= "selected";
+        } 
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    });
 }
+
+const updateFlag =(element)=>{
+        let currCode = element.value;
+        let countryCode = countryList[currCode];
+        let newSrc =`https://flagsapi.com/${countryCode}/flat/64.png`;
+        let img =element.parentElement.querySelector("img");
+        img.src = newSrc;
+};
+
+
+btn.addEventListener("click" ,(evt) =>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amVal = amount.value;
+    if(amVal ===""|| amVal<1){
+        amVal = 1;
+        amount.value = "1";
+    } 
+    
+});
--- REACT-DAY/rbmi/src/App.js
+++ REACT-DAY/rbmi/src/App.js
@@ -1,16 +1,14 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+ 
 
 function App() {
-  // const hi="hello"
-  const hi=true
   
+  
+    
   return (
-    <div>
-      {hi?<h1>my name saurabh</h1>:<h6>hello</h6>}
-
-    </div>
-
+        
+  
   );
 }
 export default App
