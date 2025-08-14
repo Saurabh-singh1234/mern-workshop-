f9ee879b5bdbb7bd2fe7b0b0fd42da4094c6c164
Saurabh Singh <saurabhsingh10761@gmail.com>
2025-08-14 10:19:28 +0530

    Refactor App component to remove unused state and render logic

--- REACT-DAY/rbmi/src/App.js
+++ REACT-DAY/rbmi/src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 
 function App() {
   
-  const li =["apple","mango","banna"]
-  const [data,setData]=useState("logged in")
-    
+     
   return (
-        <div class="my">
-          <form>
-           <ul>
-            {li.map(element=>(
-                <li>{element}</li>
-            ))}
-           </ul>
-          </form>
-      </div>        
+        <div></div>      
   
   );
 }
+
 export default App
